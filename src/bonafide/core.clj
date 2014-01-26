67131cffd199f2603a4c5a27c2aafbe22b195b76
Adam Groves <adam.groves@gmail.com>
2014-01-26 18:35:41 +0100

    Refactoring before moving more logic into build-validation

--- src/bonafide/core.clj
+++ src/bonafide/core.clj
@@ -5,19 +5,31 @@
 (declare build-validation)
 
 (defn validate-present [attr & {:as options}]
-  (build-validation attr (or options {:message "Cannot be blank"})
-                    (fn [content options]
-                      (not (clojure.string/blank? content))
-                      )
-                    )
+  (let [
+        defaults {:message "Cannot be blank"}
+
+        options (if options (merge defaults options) defaults)
+
+        condition (fn [content options]
+                    (not (clojure.string/blank? content)))
+        ]
+    (build-validation attr options condition)
+    )
   )
 
 (defn validate-email [attr & {:as options}]
-  (build-validation attr (or options {:message "Not a valid email address"})
-                    (fn [content options]
-                      (and (not (clojure.string/blank? content)) (re-find #".+@.+\..+" content))
-                      )
-                    )
+  (let [
+        defaults {:message "Not a valid email address"}
+
+        options (if options (merge defaults options) defaults)
+
+        condition (fn [content options]
+                    (and
+                     (not (clojure.string/blank? content))
+                     (re-find #".+@.+\..+" content)))
+        ]
+    (build-validation attr options condition)
+    )
   )
 
 
@@ -27,15 +39,10 @@
                attrs
                (merge-with merge-with-concat attrs {:errors {attr (options :message)}})
                ))
-           attr options
-           )
-  )
-
+           attr options))
 
 (defn- merge-with-concat [& args]
-  (apply merge-with concat-array args)
-  )
+  (apply merge-with concat-array args))
 
 (defn- concat-array [& args]
-  (flatten (into [] (concat args))
-  ))
+  (flatten (into [] (concat args))))
--- test/bonafide/core_test.clj
+++ test/bonafide/core_test.clj
@@ -3,59 +3,45 @@
         bonafide.core))
 
 (defn error-on [validation-check]
-  (keys (:errors validation-check))
-  )
+  (keys (:errors validation-check)))
 
 (deftest validate-present-test
   (testing "attribute is present"
-    (is (= (:errors ((validate-present :name) {:name "Peter"})) nil))
-    )
+    (is (= (:errors ((validate-present :name) {:name "Peter"})) nil)))
 
   (testing "attribute isn't present"
-    (is (= (error-on ((validate-present :name) {})) [:name] ))
-    )
+    (is (= (error-on ((validate-present :name) {})) [:name] )))
 
   (testing "attribute is empty string"
-    (is (= (error-on ((validate-present :name) {:name ""})) [:name]))
-    )
+    (is (= (error-on ((validate-present :name) {:name ""})) [:name])))
 
   (testing "attribute is blank string"
-    (is (= (error-on ((validate-present :name) {:name "\t\n\r "})) [:name]))
-    )
+    (is (= (error-on ((validate-present :name) {:name "\t\n\r "})) [:name])))
 
   (testing "optional message"
-    (is (= (:errors ((validate-present :name :message "WAT") {:name "\t\n\r "})) {:name "WAT"}))
-    )
-  )
+    (is (= (:errors ((validate-present :name :message "WAT") {:name "\t\n\r "})) {:name "WAT"}))))
 
 (deftest validate-email-test
   (testing "email is valid"
-    (is (= (:errors ((validate-email :email) {:email "foo@bar.com"})) nil))
-    )
+    (is (= (:errors ((validate-email :email) {:email "foo@bar.com"})) nil)))
 
   (testing "email is invalid"
-    (is (= (error-on ((validate-email :email) {:email "foo@b"})) [:email]))
-    )
+    (is (= (error-on ((validate-email :email) {:email "foo@b"})) [:email])))
 
   (testing "optional message"
-    (is (= (:errors ((validate-email :email :message "WAT") {:email "foo@"})) {:email "WAT"}))
-    )
+    (is (= (:errors ((validate-email :email :message "WAT") {:email "foo@"})) {:email "WAT"})))
 
   (testing "email is invalid"
-    (is (= (error-on ((validate-email :email) {})) [:email]))
-    )
-  )
+    (is (= (error-on ((validate-email :email) {})) [:email]))))
 
 (deftest multiple-validations-test
   (let [validate (comp
                       (validate-present :email)
                       (validate-email :email)
-                      (validate-present :name)
-                      )
-        ]
+                      (validate-present :name))]
+
     (testing "multiple validations are valid"
-      (is (= (:errors (validate {:name "John" :email "foo@bar.com"})) nil))
-      )
+      (is (= (:errors (validate {:name "John" :email "foo@bar.com"})) nil)))
 
     (testing "multiple validations are invalid"
       (is (= (:errors (validate {})) {:name "Cannot be blank" :email ["Not a valid email address", "Cannot be blank"]}))
@@ -63,16 +49,23 @@
     )
   )
 
-
-(defn validate-format [attr & {:as options}]
-  (build-validation attr (or options {:message "Wrong format"})
-                    (fn [content options]
-                      (clojure.string/blank? (re-find (options :match) content))
-                      )
-                    )
-  )
-
 (deftest custom-validation
+
+  (defn validate-format [attr & {:as options}]
+    (let [
+          defaults {:message "Wrong format"}
+
+          options (if options (merge defaults options) defaults)
+
+          condition (fn [content options]
+                      (re-find (options :match) content))
+          ]
+      (build-validation attr options condition)
+      )
+    )
+
   (testing "content matches expression"
-    (is (= (:errors ((validate-format :name :match #"ete") {:name "Peter"}) nil)))
-  ))
+    (is (= (:errors ((validate-format :name :match #"ete") {:name "Peter"})) nil)))
+
+  (testing "content doesn't expression"
+    (is (= (:errors ((validate-format :name :match #"ete") {:name "John"})) {:name "Wrong format"}))))
